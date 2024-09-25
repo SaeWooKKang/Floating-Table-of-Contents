569600eb495bfe4f300bf8ccdc7765f0deaea695
saewookkang <pjy9665@kakao.com>
2024-09-25 17:52:25 +0900

    style: 헤더영역 마우스 커서 추가

--- src/contentScript/application/src/features/FloatingToc/Header.tsx
+++ src/contentScript/application/src/features/FloatingToc/Header.tsx
@@ -1,20 +1,33 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   onPointerDown: (e: React.PointerEvent<HTMLDivElement>) => void
   hideIcon: boolean
 }
 export const Header = (props: Props) => {
+  const [isGrabbing, setIsGrabbing] = useState(false);
+
+  const handlePointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+    setIsGrabbing(true);
+    props.onPointerDown(e);
+  };
+
+  const handlePointerUp = () => {
+    setIsGrabbing(false);
+  };
+
   return (
     <motion.div 
       layout
-      onPointerDown={props.onPointerDown}
+      onPointerDown={handlePointerDown}
+      onPointerUp={handlePointerUp}
       className="!bg-white flex justify-center flex-col"
       style={{
         display: 'flex',
         alignItems: 'center',
         flexDirection: 'column',
+        cursor: isGrabbing ? 'grabbing' : 'grab',
       }}
     >
       <h1
@@ -23,12 +36,13 @@ export const Header = (props: Props) => {
           position: 'relative',
         }}
       >
-        <span style={{position: 'absolute', left: -15, display: props.hideIcon ? 'none' : 'block'}}>☁️</span> Floating Table of Contents
+        <span style={{position: 'absolute', left: -15, display: props.hideIcon ? 'none' : 'block'}}>☁️</span> 
+        <span>Floating Table of Contents</span>
       </h1>
 
       <div 
         style={{
-          height:'1px',
+          height:'0.5px',
           width: 'calc(100% - 20px)',
           backgroundColor: '#D5D5D5',
         }}
