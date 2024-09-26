1d94b66ad114c2e84a5dbaff5ce2476d02e8990d
saewookkang <pjy9665@kakao.com>
2024-09-26 09:34:42 +0900

    refactor: 이름 변경 header -> heading

--- src/contentScript/application/src/features/FloatingToc/Toc.tsx
+++ src/contentScript/application/src/features/FloatingToc/Toc.tsx
@@ -6,41 +6,42 @@ interface Props {
   showBigger: boolean
 }
 
-type HeaderInfo = {
+type HeadingInfo = {
   text: string
   id: string
   level: number
 }
 
 export const Toc = (props: Props) => {
-  const [headers, setHeaders] = useState<HeaderInfo[] | null>(null);
+  const [headingInfo, setHeadingInfo] = useState<HeadingInfo[] | null>(null);
+
 
   useEffect(() => {
     const main = document.querySelector('main')
 
-    const headers = main 
+    const headings = main 
       ? main.querySelectorAll('h1, h2, h3, h4') 
       : document.querySelectorAll('h1, h2, h3, h4');
 
-    headers.forEach((header, index) => {
-      if (header.id !== 'toc-title') {
-        header.id = `toc-header-${index}`
+    headings.forEach((heading, index) => {
+      if (heading.id !== 'toc-title') {
+        heading.id = `toc-heading-${index}`
       }
     })
     
-    const info: HeaderInfo[] = [...headers]
-      .filter(header => header.id !== 'toc-title')
-      .map((header, index) => {
-        const [_, level] = [...header.tagName]
+    const info: HeadingInfo[] = [...headings]
+      .filter(heading => heading.id !== 'toc-title')
+      .map((heading, index) => {
+        const [_, level] = [...heading.tagName]
 
         return ({
-          text: header.textContent ?? '',
-          id: `toc-header-${index}`,
+          text: heading.textContent ?? '',
+          id: `toc-heading-${index}`,
           level: Number(level)
         })
       })
 
-    setHeaders(info)
+    setHeadingInfo(info)
   }, [])
 
   return (
@@ -50,18 +51,18 @@ export const Toc = (props: Props) => {
       style={{height: 'calc(100% - 55px)', width: '100%', padding: '20px 10px 10px', outline: 'none', overflow: 'auto'}}
     >
       <ul style={{fontSize: props.showBigger ? '16px': '13px',}}>
-        {headers?.map((header) => {
+        {headingInfo?.map((headingInfo) => {
           return (
             <motion.li 
-              key={header.id} 
-              style={{paddingLeft: header.level * 10}} 
+              key={headingInfo.id} 
+              style={{paddingLeft: headingInfo.level * 10}} 
              
             >
               <a 
-                href={`#${header.id}`}
+                href={`#${headingInfo.id}`}
                 onClick={e => e.stopPropagation()}
               >
-                {header.text}
+                {headingInfo.text}
               </a>
             </motion.li>
           )
