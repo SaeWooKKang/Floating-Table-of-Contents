d47870f2dbf97c714ecd9979ca6ad1373d08436b
saewookkang <pjy9665@kakao.com>
2024-10-02 15:03:27 +0900

    fix(tailwind): 모든 하위 디렉토리가 포함되도록 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/contentScript/application/src/*.{js,ts,jsx,tsx}'],
+  content: ['./src/contentScript/application/src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
