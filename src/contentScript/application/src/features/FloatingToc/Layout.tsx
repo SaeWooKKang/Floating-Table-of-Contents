5382d3cea7d19e94e09d9c8876539a40337ba6c7
saewookkang <pjy9665@kakao.com>
2024-10-03 15:08:35 +0900

    design(Layout): z index 수정

--- src/contentScript/application/src/features/FloatingToc/Layout.tsx
+++ src/contentScript/application/src/features/FloatingToc/Layout.tsx
@@ -2,7 +2,7 @@ export const Layout = (props: {
   children: React.ReactNode
 }) => {
   return (
-    <div className="absolute top-0 left-0 w-full overflow-hidden pointer-events-none z-1000">
+    <div className="absolute top-0 left-0 w-full overflow-hidden pointer-events-none z-[1000]">
       {props.children}
     </div>
   )
