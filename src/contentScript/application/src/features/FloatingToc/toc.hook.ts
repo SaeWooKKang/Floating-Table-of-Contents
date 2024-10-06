236c93b99ac74c86d767e3722c5aedf20f44879d
saewookkang <pjy9665@kakao.com>
2024-10-06 22:03:46 +0900

    feat(highlight): 스크롤 역방향 하이라이트 변경 기능 추가

--- src/contentScript/application/src/features/FloatingToc/toc.hook.ts
+++ src/contentScript/application/src/features/FloatingToc/toc.hook.ts
@@ -24,6 +24,9 @@ export const useTocHighlight = ({
 }) => {
   const [activeId, setActiveId] = useState<string | null>(null)
   const observerRef = useRef<IntersectionObserver | null>(null)
+  const prevScrollY = useRef(0)
+
+  const headingEntries = useRef<Array<IntersectionObserverEntry>>([])
 
   const options = {
     rootMargin: '-80px 0px -60% 0px',
@@ -37,10 +40,37 @@ export const useTocHighlight = ({
     [],
   )
 
+  useEffect(() => {
+    prevScrollY.current = document.documentElement.scrollTop ?? 0
+  }, [])
+
   const handleIntersection = useCallback((entries: IntersectionObserverEntry[]) => {
+    const isScrollingUp = (document.documentElement.scrollTop ?? 0) < (prevScrollY?.current ?? 0)
+    prevScrollY.current = document.documentElement.scrollTop ?? 0
+
     for (const entry of entries) {
       if (entry.isIntersecting) {
-        setActiveId(entry.target.id)
+        if (!headingEntries.current.find((h) => h.target.id === entry.target.id)) {
+          headingEntries.current = [...headingEntries.current, entry]
+          setActiveId(entry.target.id)
+
+          break
+        }
+      }
+
+      const foundEntryIndex = headingEntries.current.findIndex(
+        (h) => h.target.id === entry.target.id,
+      )
+
+      if (
+        !entry.isIntersecting &&
+        foundEntryIndex !== -1 &&
+        foundEntryIndex === headingEntries.current.length - 1 &&
+        isScrollingUp
+      ) {
+        headingEntries.current = headingEntries.current.slice(0, -1)
+        setActiveId(headingEntries.current[headingEntries.current.length - 1]?.target.id)
+
         break
       }
     }
