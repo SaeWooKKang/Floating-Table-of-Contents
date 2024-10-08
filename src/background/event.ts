3331d9c22e8e1b5072baa20632c351b65a6570e9
JaeYoung <87258182+SaeWooKKang@users.noreply.github.com>
2024-10-08 10:22:14 +0900

    feat: context menu를 통한 toggle 지원 (#19)
    * chore(manifest.json): conextMenus 권한 추가
    
    * feat(contextMenus): 컨텍스트메뉴로 토글 기능 추가
    
    * refactor: 중복 함수로 분리
    
    * chore(readme.md): 토글 방법 추가

--- src/background/event.ts
+++ src/background/event.ts
@@ -1,13 +1,39 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeBackgroundColor({ color: '#f4fb04' })
   chrome.action.setBadgeText({ text: '' })
+
+  chrome.contextMenus.create({
+    id: CONTEXT_MENU_ID,
+    title: 'Toggle Floating ToC',
+    contexts: ['all'],
+    documentUrlPatterns: URL_PATTERNS,
+  })
 })
 
-chrome.action.onClicked.addListener(async (tab) => {
-  if (!tab.url?.startsWith('http') || !tab.id) {
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId !== CONTEXT_MENU_ID) {
     return
   }
 
+  if (!isValidTab(tab) || !isValidUrl(tab.url)) {
+    return
+  }
+
+  toggleExtension(tab)
+})
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (!isValidTab(tab) || !isValidUrl(tab.url)) {
+    return
+  }
+
+  toggleExtension(tab)
+})
+
+const CONTEXT_MENU_ID = 'Floating-toc'
+const URL_PATTERNS = ['http://*/*', 'https://*/*']
+
+const toggleExtension = async (tab: chrome.tabs.Tab) => {
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id })
   const nextState = prevState === 'on' ? '' : 'on'
 
@@ -21,4 +47,11 @@ chrome.action.onClicked.addListener(async (tab) => {
   } else {
     chrome.tabs.sendMessage(tab.id ?? chrome.tabs.TAB_ID_NONE, { status: 'off' })
   }
-})
+}
+
+const isValidUrl = (url?: string) =>
+  URL_PATTERNS.some((pattern) => new RegExp(pattern).test(url ?? ''))
+
+const isValidTab = (tab?: chrome.tabs.Tab): tab is chrome.tabs.Tab => {
+  return Boolean(tab?.id)
+}
