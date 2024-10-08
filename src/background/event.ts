03015ba406ecacdbdbaafc8605042fb2bc2ccdde
saewookkang <pjy9665@kakao.com>
2024-10-08 10:07:05 +0900

    refactor: 중복 함수로 분리

--- src/background/event.ts
+++ src/background/event.ts
@@ -3,41 +3,37 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({ text: '' })
 
   chrome.contextMenus.create({
-    id: 'Floating-toc',
+    id: CONTEXT_MENU_ID,
     title: 'Toggle Floating ToC',
     contexts: ['all'],
+    documentUrlPatterns: URL_PATTERNS,
   })
 })
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (!tab?.url?.startsWith('http') || !tab.id) {
+  if (info.menuItemId !== CONTEXT_MENU_ID) {
     return
   }
 
-  if (info.menuItemId !== 'Floating-toc') {
+  if (!isValidTab(tab) || !isValidUrl(tab.url)) {
     return
   }
 
-  const prevState = await chrome.action.getBadgeText({ tabId: tab.id })
-  const nextState = prevState === 'on' ? '' : 'on'
-
-  await chrome.action.setBadgeText({
-    tabId: tab.id,
-    text: nextState,
-  })
-
-  if (nextState === 'on') {
-    chrome.tabs.sendMessage(tab.id ?? chrome.tabs.TAB_ID_NONE, { status: 'on' })
-  } else {
-    chrome.tabs.sendMessage(tab.id ?? chrome.tabs.TAB_ID_NONE, { status: 'off' })
-  }
+  toggleExtension(tab)
 })
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (!tab.url?.startsWith('http') || !tab.id) {
+  if (!isValidTab(tab) || !isValidUrl(tab.url)) {
     return
   }
 
+  toggleExtension(tab)
+})
+
+const CONTEXT_MENU_ID = 'Floating-toc'
+const URL_PATTERNS = ['http://*/*', 'https://*/*']
+
+const toggleExtension = async (tab: chrome.tabs.Tab) => {
   const prevState = await chrome.action.getBadgeText({ tabId: tab.id })
   const nextState = prevState === 'on' ? '' : 'on'
 
@@ -51,4 +47,11 @@ chrome.action.onClicked.addListener(async (tab) => {
   } else {
     chrome.tabs.sendMessage(tab.id ?? chrome.tabs.TAB_ID_NONE, { status: 'off' })
   }
-})
+}
+
+const isValidUrl = (url?: string) =>
+  URL_PATTERNS.some((pattern) => new RegExp(pattern).test(url ?? ''))
+
+const isValidTab = (tab?: chrome.tabs.Tab): tab is chrome.tabs.Tab => {
+  return Boolean(tab?.id)
+}
