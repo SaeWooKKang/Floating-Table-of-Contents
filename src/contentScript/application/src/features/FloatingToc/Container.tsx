ac4ec07f00116986843826a2897e542bda4960cf
saewookkang <pjy9665@kakao.com>
2024-10-28 17:25:23 +0900

    feat(contentsScript): 초기 위치 viewport로 제한

--- src/contentScript/application/src/features/FloatingToc/Container.tsx
+++ src/contentScript/application/src/features/FloatingToc/Container.tsx
@@ -11,9 +11,11 @@ import { Layout } from './Layout'
 import { Divider } from '../../components/Divider'
 import { useExternalActions, useInitialPosition } from '../../store/external'
 import { TOC_INITIAL_STATE, tocReducer } from './toc.reducer'
+import type { Area } from './toc.type'
+import { parseInitialPosition } from './toc.utils'
 
 const Container = () => {
-  const [constraints, setConstraints] = useState({ left: 0, right: 0, top: 0, bottom: 0 })
+  const [constraints, setConstraints] = useState<Area | null>(null)
   const [toc, dispatch] = useReducer(tocReducer, TOC_INITIAL_STATE)
 
   const position = useInitialPosition()
@@ -24,11 +26,14 @@ const Container = () => {
   const controls = useDragControls()
 
   useEffect(() => {
-    setConstraints((prev) => ({
-      ...prev,
+    const constraints = {
+      left: 0,
+      top: 0,
       bottom: window.innerHeight - toc.size.height,
       right: document.documentElement.scrollWidth - toc.size.width,
-    }))
+    }
+
+    setConstraints(constraints)
   }, [toc])
 
   const handlePointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
@@ -38,6 +43,13 @@ const Container = () => {
   const handleTap = () => {
     dispatch({ type: showBigger ? 'smaller' : 'bigger' })
   }
+  const hasMounted = constraints !== null
+
+  if (!hasMounted) {
+    return null
+  }
+
+  const parsedInitialPosition = parseInitialPosition(position, constraints)
 
   return (
     <Layout>
@@ -45,7 +57,7 @@ const Container = () => {
         constraints={constraints}
         controls={controls}
         tocSize={toc.size}
-        initialPosition={position}
+        initialPosition={parsedInitialPosition}
         onDragEnd={changePosition}
       >
         <Header onPointerDown={handlePointerDown} showBigger={showBigger} />
