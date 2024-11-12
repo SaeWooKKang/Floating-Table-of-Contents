c8bd03acbcc495b38d793b0a00d3b13c17ccf14a
JaeYoung <87258182+SaeWooKKang@users.noreply.github.com>
2024-11-12 17:41:08 +0900

    feature: heading 수집 필터링 추가 (#25)
    * chore: 사용하지 않는 코드 삭제
    
    * refactor: heading filtering 조건 추가하기 위해 분리
    
    * refactor: heading container 함수로 분리 및 selector 추가
    
    * feat(filterHeadingList): 필터링 추가(&nbsp;)
    
    - 문제 url: https://aws.amazon.com/ko/compare/the-difference-between-https-and-http/
    
    * feat(filterHeadingList): header, footer 필터링 추가

--- src/contentScript/application/src/features/FloatingToc/toc.const.ts
+++ src/contentScript/application/src/features/FloatingToc/toc.const.ts
@@ -1,1 +1,7 @@
 export const TOC_TITLE_ID = 'toc-title'
+
+export const HEADING_SELECTOR_ORDERER = [
+  'main',
+  'body article'
+] as const
+export const NONE_BREAKING_SPACE_UNICODE = '\u00A0'
--- src/contentScript/application/src/features/FloatingToc/toc.utils.ts
+++ src/contentScript/application/src/features/FloatingToc/toc.utils.ts
@@ -1,21 +1,39 @@
+import { HEADING_SELECTOR_ORDERER, NONE_BREAKING_SPACE_UNICODE, TOC_TITLE_ID } from './toc.const';
 import type { Area, HeadingInfo } from './toc.type'
 
+const getHeadingContainer = () => {
+  for (const selector of HEADING_SELECTOR_ORDERER)  {
+    const container = document.querySelector(selector)
+
+    if (container) {
+      return container
+    }
+  }
+
+  return document
+}
+
 export const getAllHeadings = () => {
-  const main = document.querySelector('main')
+  const container = getHeadingContainer()
+  const headings = container
+    .querySelectorAll<HTMLHeadingElement>('h1, h2, h3, h4')
+    
+  return Array.from(headings)
+}
 
-  const headings = main
-    ? main.querySelectorAll<HTMLHeadingElement>('h1, h2, h3, h4')
-    : document.querySelectorAll<HTMLHeadingElement>('h1, h2, h3, h4')
-
+const filterHeadingList = (headings: Array<Element>)=> {
   return headings
+    .filter(headings => {
+      return !headings.closest('header') && !headings.closest('footer')
+    })
+    .filter((heading) => heading.id !== TOC_TITLE_ID)
+    .filter(heading => heading.textContent !== '')
+    .filter(heading => heading.textContent !== NONE_BREAKING_SPACE_UNICODE)
 }
 
 export const parseHeadingInfo = (headings: Array<Element>): HeadingInfo[] => {
-  headings.forEach((heading, index) => {
-    heading.id = `toc-heading-${index}`
-  })
-
-  const info: HeadingInfo[] = [...headings]
+  const filteredHeadingList = filterHeadingList(headings)
+  const info: HeadingInfo[] = filteredHeadingList
     .map((heading, index) => {
       const [_, level] = [...heading.tagName]
 
@@ -25,7 +43,11 @@ export const parseHeadingInfo = (headings: Array<Element>): HeadingInfo[] => {
         level: Number(level),
       }
     })
-    .filter((headingInfo) => headingInfo.text !== '')
+
+    filteredHeadingList.forEach((heading, index) => {
+      heading.id = `toc-heading-${index}`
+    })
+    
 
   return info
 }
